834988b4450cb16d623160a2ea9b5cedfae61958
Sourav <souravraj664@gmail.com>
2024-03-17 19:37:27 +0530

    Overall Analysis

--- app.py
+++ app.py
@@ -5,7 +5,49 @@ import matplotlib.pyplot as plt
 
 st.set_page_config(layout='wide', page_title='Startup Analysis')
 df = pd.read_csv('startup_v1.csv', index_col=False).drop(columns=['Unnamed: 0'])
+df['month'] = pd.to_datetime(df['date']).dt.month
 
+def load_overall_analysis():
+    st.title('Overall Analysis')
+    
+    # total invested amount
+    total = round(df['amount'].sum())
+    
+    # max amount infused in a startup
+    max_funding = df.groupby('startup')['amount'].max().sort_values(ascending=False).head(1).values[0]
+    
+    # avg ticket size
+    avg_funding = df.groupby('startup')['amount'].sum().mean()
+    
+    #Total Funded Startup
+    startup = df['startup'].nunique()
+    
+    col1,col2,col3,col4 = st.columns(4)
+    with col1:
+       st.metric('Total',str(total) + ' Cr')
+    with col2:
+        st.metric('Max',str(round(max_funding)) + ' Cr')
+    with col3:
+        st.metric('Avg',str(round(avg_funding)) + ' Cr') 
+        
+    with col4:
+        st.metric('StartUp',str(startup))  
+    
+    st.header('MoM graph')    
+    selected_option = st.selectbox('Select Type',['Total','Count'])
+    
+    if selected_option == 'Total': 
+        temp_df = df.groupby(['year','month'], as_index=False)['amount'].sum() 
+        
+    else:
+        temp_df = df.groupby(['year','month'], as_index=False)['amount'].count() 
+    
+    temp_df['x_axis'] = temp_df['month'].astype('str') + '-' + temp_df['year'].astype('str')
+    fig, ax = plt.subplots()
+    ax.plot(temp_df['x_axis'], temp_df['amount'])
+
+    st.pyplot(fig)
+        
 def load_investor_details(investor):
     st.title(investor)
     # load the recent 5 investments of the investor
@@ -21,12 +63,29 @@ def load_investor_details(investor):
         ax.bar(big5_series.index, big5_series.values)
         st.pyplot(fig)
         
-
+    with col2:
+        vertical5_series = df[df['investors'].str.contains(investor)].groupby('vertical')['amount'].sum().sort_values(ascending=False).head(10)
+        st.subheader('Top 10 Sectors Invested in')
+        fig, ax = plt.subplots()
+        ax.pie(vertical5_series,labels=vertical5_series.index, autopct='%.2f%%')
+        st.pyplot(fig)
+        
+    
+    year_series = df[df['investors'].str.contains(investor)].groupby('year')['amount'].sum()
+    st.subheader('YoY Investments')
+    fig, ax = plt.subplots()
+    ax.plot(year_series)
+    ax.set_xlim(2014,2020)
+    st.pyplot(fig)
+    
+    
 st.sidebar.title('Startup Funding Analyis')
 option = st.sidebar.selectbox('Select One',['Overall Analysis','StartUp','Investor'])
 
 if option == 'Overall Analysis':
-    pass
+    # btn0 = st.sidebar.button('Show Overall Analysis')
+    # if btn0:
+        load_overall_analysis()
 
 elif option == 'StartUp':
     st.sidebar.selectbox('Select StartUp',sorted(df['startup'].unique().tolist()))
